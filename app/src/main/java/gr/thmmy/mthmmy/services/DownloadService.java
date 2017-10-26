8962f9fa548997043b191e82ae9ae110171aa5a6
Ezerous <ezerous@gmail.com>
2017-10-26 11:26:48 +0300

    Up Gradle Plugin and Firebase Disabled Firebase for Debug build (also removed associated google-services.json)

--- app/src/main/java/gr/thmmy/mthmmy/services/DownloadService.java
+++ app/src/main/java/gr/thmmy/mthmmy/services/DownloadService.java
@@ -7,7 +7,6 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Environment;
 import android.support.annotation.NonNull;
-import android.util.Log;
 import android.webkit.MimeTypeMap;
 
 import java.io.File;
