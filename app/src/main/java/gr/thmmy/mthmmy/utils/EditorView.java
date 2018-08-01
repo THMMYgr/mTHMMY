8677f8dec81f2422e4d9a64187245f6bef8278f3
Thodoris1999 <teotyrov@gmail.com>
2018-08-01 12:09:08 +0300

    EditorView init

---
+++ app/src/main/java/gr/thmmy/mthmmy/utils/EditorView.java
@@ -0,0 +1,10 @@
+package gr.thmmy.mthmmy.utils;
+
+import android.content.Context;
+import android.widget.LinearLayout;
+
+public class EditorView extends LinearLayout {
+    public EditorView(Context context) {
+        super(context);
+    }
+}
