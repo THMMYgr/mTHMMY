c89117c88439eeb36434edd227d39d9a51b8e19d
Thodoris1999 <teotyrov@gmail.com>
2018-08-25 12:38:16 +0300

    color picker

--- app/src/main/java/gr/thmmy/mthmmy/utils/EditorView.java
+++ app/src/main/java/gr/thmmy/mthmmy/utils/EditorView.java
@@ -2,22 +2,24 @@ package gr.thmmy.mthmmy.utils;
 
 import android.app.Activity;
 import android.content.Context;
-import android.content.res.Resources;
 import android.support.v7.widget.AppCompatImageButton;
 import android.text.Editable;
 import android.util.AttributeSet;
-import android.util.TypedValue;
+import android.util.SparseArray;
 import android.view.LayoutInflater;
 import android.view.inputmethod.EditorInfo;
 import android.view.inputmethod.InputConnection;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
-import android.widget.GridLayout;
 import android.widget.LinearLayout;
+import android.widget.PopupWindow;
 
 import gr.thmmy.mthmmy.R;
 
 public class EditorView extends LinearLayout {
+
+    private SparseArray<String> colors = new SparseArray<>();
+    private SparseArray<String> fonts = new SparseArray<>();
 
     private EditText editText;
     private AppCompatImageButton emojiButton;
@@ -93,18 +95,51 @@ public class EditorView extends LinearLayout {
             getText().insert(editText.getSelectionStart(), "[s][/s]");
             editText.setSelection(editText.getSelectionStart() - 4);
         });
-        // TODO: popup menu for colors
+
+        colors.append(R.id.black, "black");
+        colors.append(R.id.red, "red");
+        colors.append(R.id.yellow, "yellow");
+        colors.append(R.id.pink, "pink");
+        colors.append(R.id.green, "green");
+        colors.append(R.id.orange, "orange");
+        colors.append(R.id.purple, "purple");
+        colors.append(R.id.blue, "blue");
+        colors.append(R.id.beige, "beige");
+        colors.append(R.id.brown, "brown");
+        colors.append(R.id.teal, "teal");
+        colors.append(R.id.navy, "navy");
+        colors.append(R.id.maroon, "maroon");
+        colors.append(R.id.lime_green, "limegreen");
+
         findViewById(R.id.text_color_button).setOnClickListener(view -> {
-            if (editText.hasSelection())
-                editText.getText().delete(editText.getSelectionStart(), editText.getSelectionEnd());
-            getText().insert(editText.getSelectionStart(), "[color=][/color]");
-            editText.setSelection(editText.getSelectionStart() - 8);
+            PopupWindow popupWindow = new PopupWindow(view.getContext());
+            popupWindow.setHeight(LayoutParams.WRAP_CONTENT);
+            popupWindow.setWidth(LayoutParams.WRAP_CONTENT);
+            popupWindow.setFocusable(true);
+            LinearLayout colorPicker = (LinearLayout) LayoutInflater.from(context).inflate(R.layout.editor_view_color_picker, null);
+            popupWindow.setContentView(colorPicker);
+            for (int i = 0; i < colorPicker.getChildCount(); i++) {
+                colorPicker.getChildAt(i).setOnClickListener(v -> {
+                    if (editText.hasSelection())
+                        editText.getText().delete(editText.getSelectionStart(), editText.getSelectionEnd());
+                    getText().insert(editText.getSelectionStart(), "[color=" + colors.get(v.getId()) + "][/color]");
+                    editText.setSelection(editText.getSelectionStart() - 8);
+                    popupWindow.dismiss();
+                });
+            }
+            popupWindow.showAsDropDown(view);
         });
         findViewById(R.id.text_size_button).setOnClickListener(view -> {
             if (editText.hasSelection())
                 editText.getText().delete(editText.getSelectionStart(), editText.getSelectionEnd());
             getText().insert(editText.getSelectionStart(), "[size=10pt][/size]");
             editText.setSelection(editText.getSelectionStart() - 7);
+        });
+        findViewById(R.id.unordered_list_button).setOnClickListener(view -> {
+            if (editText.hasSelection())
+                editText.getText().delete(editText.getSelectionStart(), editText.getSelectionEnd());
+            getText().insert(editText.getSelectionStart(), "[list]\n[li][/li]\n[li][/li]\n[/list]");
+            editText.setSelection(editText.getSelectionStart() - 23);
         });
         findViewById(R.id.align_left_button).setOnClickListener(view -> {
             if (editText.hasSelection())
