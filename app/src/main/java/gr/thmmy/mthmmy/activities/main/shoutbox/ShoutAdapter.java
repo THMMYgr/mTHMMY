db9c800743e0f62e9e021c6d599af992e98565b3
Thodoris1999 <teotyrov@gmail.com>
2018-10-04 19:36:53 +0300

    fill in the adapter class

--- app/src/main/java/gr/thmmy/mthmmy/activities/main/shoutbox/ShoutAdapter.java
+++ app/src/main/java/gr/thmmy/mthmmy/activities/main/shoutbox/ShoutAdapter.java
@@ -1,30 +1,64 @@
 package gr.thmmy.mthmmy.activities.main.shoutbox;
 
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
 import androidx.annotation.NonNull;
+import gr.thmmy.mthmmy.R;
+import gr.thmmy.mthmmy.activities.board.BoardActivity;
+import gr.thmmy.mthmmy.activities.profile.ProfileActivity;
+import gr.thmmy.mthmmy.activities.topic.TopicActivity;
 import gr.thmmy.mthmmy.model.Shout;
+import gr.thmmy.mthmmy.model.ThmmyPage;
 import gr.thmmy.mthmmy.utils.CustomRecyclerView;
 
+import static android.content.Intent.FLAG_ACTIVITY_NEW_TASK;
+import static gr.thmmy.mthmmy.activities.board.BoardActivity.BUNDLE_BOARD_TITLE;
+import static gr.thmmy.mthmmy.activities.board.BoardActivity.BUNDLE_BOARD_URL;
+import static gr.thmmy.mthmmy.activities.profile.ProfileActivity.BUNDLE_PROFILE_THUMBNAIL_URL;
+import static gr.thmmy.mthmmy.activities.profile.ProfileActivity.BUNDLE_PROFILE_URL;
+import static gr.thmmy.mthmmy.activities.profile.ProfileActivity.BUNDLE_PROFILE_USERNAME;
+import static gr.thmmy.mthmmy.activities.topic.TopicActivity.BUNDLE_TOPIC_URL;
+
 public class ShoutAdapter extends CustomRecyclerView.Adapter<ShoutAdapter.ShoutViewHolder> {
+    private Context context;
     private ArrayList<Shout> shouts;
 
-    public ShoutAdapter(ArrayList<Shout> shouts) {
+    public ShoutAdapter(Context context, ArrayList<Shout> shouts) {
+        this.context = context;
         this.shouts = shouts;
     }
 
     @NonNull
     @Override
     public ShoutViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.shout, parent, false);
+        return new ShoutViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull ShoutViewHolder holder, int position) {
-
+        Shout currentShout = shouts.get(position);
+        holder.author.setText(currentShout.getShouter());
+        holder.dateTime.setText(currentShout.getDate());
+        holder.shoutContent.setClickable(true);
+        holder.shoutContent.setWebViewClient(new LinkLauncher());
+        holder.shoutContent.loadDataWithBaseURL("file:///android_asset/", currentShout.getShout(),
+                "text/html", "UTF-8", null);
     }
 
     @Override
@@ -34,8 +68,73 @@ public class ShoutAdapter extends CustomRecyclerView.Adapter<ShoutAdapter.ShoutV
 
     static class ShoutViewHolder extends CustomRecyclerView.ViewHolder {
 
-        public ShoutViewHolder(@NonNull View itemView) {
+        TextView author, dateTime;
+        WebView shoutContent;
+
+        ShoutViewHolder(@NonNull View itemView) {
             super(itemView);
+
+            author = itemView.findViewById(R.id.author_textview);
+            dateTime = itemView.findViewById(R.id.date_time_textview);
+            shoutContent = itemView.findViewById(R.id.shout_content);
+        }
+    }
+
+    class LinkLauncher extends WebViewClient {
+
+        @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+            final Uri uri = request.getUrl();
+            return handleUri(uri);
+        }
+
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            final Uri uri = Uri.parse(url);
+            return handleUri(uri);
+        }
+
+        private boolean handleUri(Uri uri) {
+            final String uriString = uri.toString();
+
+            ThmmyPage.PageCategory target = ThmmyPage.resolvePageCategory(uri);
+            if (target.is(ThmmyPage.PageCategory.TOPIC)) {
+                //This url points to a topic
+                Intent intent = new Intent(context, TopicActivity.class);
+                Bundle extras = new Bundle();
+                extras.putString(BUNDLE_TOPIC_URL, uriString);
+                intent.putExtras(extras);
+                intent.setFlags(FLAG_ACTIVITY_NEW_TASK);
+                context.startActivity(intent);
+                return true;
+            } else if (target.is(ThmmyPage.PageCategory.BOARD)) {
+                Intent intent = new Intent(context, BoardActivity.class);
+                Bundle extras = new Bundle();
+                extras.putString(BUNDLE_BOARD_URL, uriString);
+                extras.putString(BUNDLE_BOARD_TITLE, "");
+                intent.putExtras(extras);
+                intent.setFlags(FLAG_ACTIVITY_NEW_TASK);
+                context.startActivity(intent);
+                return true;
+            } else if (target.is(ThmmyPage.PageCategory.PROFILE)) {
+                Intent intent = new Intent(context, ProfileActivity.class);
+                Bundle extras = new Bundle();
+                extras.putString(BUNDLE_PROFILE_URL, uriString);
+                extras.putString(BUNDLE_PROFILE_THUMBNAIL_URL, "");
+                extras.putString(BUNDLE_PROFILE_USERNAME, "");
+                intent.putExtras(extras);
+                intent.setFlags(FLAG_ACTIVITY_NEW_TASK);
+                context.startActivity(intent);
+                return true;
+            }
+
+            Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+            intent.setFlags(FLAG_ACTIVITY_NEW_TASK);
+            context.startActivity(intent);
+
+            //Method always returns true as no url should be loaded in the WebViews
+            return true;
         }
     }
 }
