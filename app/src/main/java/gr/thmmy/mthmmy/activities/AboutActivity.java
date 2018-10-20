eb1a7f7889b8bbab9644bca00f3cf4d4e3f78b31
Ezerous <ezerous@gmail.com>
2018-10-20 21:02:21 +0300

    Grgit improvements

--- app/src/main/java/gr/thmmy/mthmmy/activities/AboutActivity.java
+++ app/src/main/java/gr/thmmy/mthmmy/activities/AboutActivity.java
@@ -50,7 +50,9 @@ public class AboutActivity extends BaseActivity {
 
         String versionInfo = "";
         if(gitExists)
-            versionInfo = "-" + BuildConfig.CURRENT_BRANCH + "-" + commitHash + " ";
+            versionInfo = "-" + BuildConfig.CURRENT_BRANCH + "-" + commitHash
+                    + (BuildConfig.IS_CLEAN ? "" : "-dirty")
+                    + " ";  // Avoid last letter being cut in italics styled TextView
 
         //Initialize appbar
         appBar = findViewById(R.id.appbar);
