bbfa78d686933fd2ff7fc5a155434ffebb0e80a2
Ezerous <ezerous@gmail.com>
2020-10-11 19:16:59 +0300

    Crash fix for certain attachments

--- app/src/main/java/gr/thmmy/mthmmy/activities/topic/TopicParser.java
+++ app/src/main/java/gr/thmmy/mthmmy/activities/topic/TopicParser.java
@@ -294,7 +294,7 @@ public class TopicParser {
                             Timber.e(e, "Attached file malformed url");
                             break;
                         }
-                        String attachedFileName = tmpAttachedFileUrlAndName.wholeText().substring(1);
+                        String attachedFileName = tmpAttachedFileUrlAndName.text();
 
                         //Gets file's info (size and download count)
                         String postAttachmentsTextSbstr = postAttachmentsText.substring(
@@ -366,7 +366,7 @@ public class TopicParser {
                             Timber.e(e, "Attached file malformed url");
                             break;
                         }
-                        String attachedFileName = tmpAttachedFileUrlAndName.wholeText().substring(1);
+                        String attachedFileName = tmpAttachedFileUrlAndName.text();
 
                         //Gets file's info (size and download count)
                         String postAttachmentsTextSbstr = postAttachmentsText.substring(
